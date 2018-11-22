3ba78fe52cffaa40360f6df99f18da4ba0539838
Petrashchuk Dmytro Ivanovych <Dimon.develop@gmail.com>
2018-11-22 14:06:56 +0200

    Added circle shapes markers to polyline direction

--- src/main/java/ua/tripguide/tripguideua/Utils/GetDirectionsData.java
+++ src/main/java/ua/tripguide/tripguideua/Utils/GetDirectionsData.java
@@ -1,15 +1,18 @@
 package ua.tripguide.tripguideua.Utils;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.Cap;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.maps.model.RoundCap;
-import com.google.android.gms.maps.model.SquareCap;
 import com.google.maps.android.PolyUtil;
 
 import org.json.JSONArray;
@@ -21,9 +24,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+
+import ua.tripguide.tripguideua.R;
 
 public class GetDirectionsData extends AsyncTask<Object, String, String> {
 
@@ -101,6 +105,19 @@ public class GetDirectionsData extends AsyncTask<Object, String, String> {
 
             }
 
+            int countPoints;
+            int height = 15;
+            int width = 15;
+            BitmapDrawable bitmapdraw_small=(BitmapDrawable) c.getResources().getDrawable(R.drawable.circle_small_white);
+            Bitmap b_small=bitmapdraw_small.getBitmap();
+            Bitmap smallMarker = Bitmap.createScaledBitmap(b_small, width, height, false);
+
+            BitmapDrawable bitmapdraw=(BitmapDrawable) c.getResources().getDrawable(R.drawable.circle_middle_black);
+            Bitmap b_middle=bitmapdraw.getBitmap();
+            Bitmap middleMarker = Bitmap.createScaledBitmap(b_middle, width*3, height*3, false);
+
+
+            ArrayList<LatLng> latLngs = new ArrayList<>();
             for (ArrayList<String> arrayList:arrayListLegs) {
                 for (String poliline:arrayList) {
                     PolylineOptions options = new PolylineOptions();
@@ -111,10 +128,21 @@ public class GetDirectionsData extends AsyncTask<Object, String, String> {
                     options.geodesic(true);
                     options.addAll(PolyUtil.decode(poliline));
 
+                    countPoints = options.getPoints().size();
+                    LatLng latLngStart = options.getPoints().get(0);
+                    LatLng latLngFinish = options.getPoints().get(countPoints-1);
+                    latLngs.add(latLngStart);
+                    latLngs.add(latLngFinish);
+
+                    if (!poliline.equals(arrayList.get(0))){
+                        mMap.addMarker(new MarkerOptions().position(latLngStart).icon(BitmapDescriptorFactory.fromBitmap(smallMarker)).snippet(String.valueOf(countPoints))
+                                .title(latLngStart.latitude + " " + latLngStart.longitude));
+                    }
                     mMap.addPolyline(options);
                 }
-
             }
+            mMap.addMarker(new MarkerOptions().position(latLngs.get(0)).icon(BitmapDescriptorFactory.fromBitmap(middleMarker)).anchor(0.5f,0.5f));
+            mMap.addMarker(new MarkerOptions().position(latLngs.get(latLngs.size()-1)).icon(BitmapDescriptorFactory.fromBitmap(middleMarker)).anchor(0.5f,0.5f));
 
         } catch (JSONException e) {
             e.printStackTrace();
